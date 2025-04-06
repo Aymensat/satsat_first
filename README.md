9762e63ac1208a65a209036a4eb9a987baefde35
Aymen Satouri <satouriaymen@gmail.com>
2025-04-06 22:11:33 +0100

    sudent component added , works fine ema not protype-ish

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
+
+
+
+Project done by noobie ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # MyProject
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.2.5.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { StudentSpaceComponent } from "./student-space/student-space.component"; 
+import { StudentScheduleComponent } from './student-schedule/student-schedule.component'; 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePageComponent, StudentSpaceComponent],
+  imports: [RouterOutlet, HomePageComponent, StudentScheduleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
